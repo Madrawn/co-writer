c04fefdf661ca102379458f0cb2d52fb66c0f68a
Daniel Dengler <daniel@busitec.de>
2025-07-26 07:27:06 +0200

    feat: add setupTests file for Vitest configuration and update tsconfig to include additional files

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    include: ['**/*.test.ts'],
+    environment: "jsdom",
+    globals: true,
+    setupFiles: 'setupTests.ts',
+    include: ["**/*.test.ts", "**/*.test.tsx"],
     coverage: {
-      reporter: ['text', 'json', 'html'],
+      reporter: ["text", "json", "html"],
     },
   },
 });
